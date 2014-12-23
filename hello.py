053b4415346bad46cc9e2a21ab38b2e8860a607b
Bob Xu <bob@bob.org>
2014-12-23 11:59:28 +0800

    hello.py modified 1

--- hello.py
+++ hello.py
@@ -29,8 +29,22 @@ while n > 0:
     sum = sum + n
 print sum
 
-birth = int(raw_input('Pls input ur birth year:'))
+#birth = int(raw_input('Pls input ur birth year:'))
+birth = 2008
 if birth > 2000:
     print '蛋蛋后'
 else:
     print '蛋蛋前'
+# 蛋蛋后
+
+family = {'anata':'xc','xfer':'hh'}
+print family['anata']
+print family.get('anata')
+print family.iterkeys()
+
+s1 = set([1,1,2,3,4,5,6,6,6,8,7])
+s2 = set([3,5,20,100,88])
+print s1
+print s2
+print s1 & s2
+print s1 | s2
